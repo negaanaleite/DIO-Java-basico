7b3235c94d52057b07a7b09c48961d0708c5aefc
Ana Leite <negaanaleite@gmail.com>
2023-09-12 14:26:48 -0300

    Aula OperadoresAritimeticos

---
+++ java-anatomia-classes/src/EDU/Ana/Projetos/Operadores.java
@@ -0,0 +1,62 @@
+package EDU.Ana.Projetos;
+
+import javax.swing.plaf.synth.SynthToolTipUI;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // String nomeCompleto = "Linguagem" + "JAVA";
+        // System.out.println(nomeCompleto);
+        // String concatenacao = "?";
+        // concatenacao = 1 + 1 + 1 + "1";
+        // System.out.println(concatenacao);
+        // concatenacao = 1 + "1" + 1 + 1;
+        // System.out.println(concatenacao);
+        // concatenacao = 1 + "1" + 1 + "1";
+        // System.out.println(concatenacao);
+        // concatenacao = "1" + 1 + 1 + 1;
+        // System.out.println(concatenacao);
+        // concatenacao = "1" + (1 + 1 + 1);
+        // int numero = 5;
+        // System.out.println( ++ numero);
+        // System.out.println(numero);
+        // boolean variavel = true;
+        // variavel= !variavel;
+        // System.out.println(!variavel);
+        // int a, b;
+        // a = 5;
+        // b = 6;
+        // String resultado = " ";
+        // if (a == b)
+        // resultado = "verdadeiro";
+        // else
+        // resultado = "falso";
+
+        // System.out.println(resultado);
+
+        // OPERADORES_ RELACIONAIS
+
+        // int numero1 = 1;
+        // int numero2 = 2;
+
+        // boolean simNao = numero1 == numero2;
+        // System.out.println("numeroUm é igual a numeroDois? "+ simNao);
+
+        // simNao = numero1 != numero2;
+        // System.out.println("numeroUm é diferente do numeroDois? "+ simNao);
+
+        // simNao = numero1 > numero2;
+        // System.out.println("numeroUm é maior do numeroDois? "+ simNao);
+
+        // *OPERADORES_LÓGIOS*
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2 ){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        System.out.println("fim");
+    }
+
+}
