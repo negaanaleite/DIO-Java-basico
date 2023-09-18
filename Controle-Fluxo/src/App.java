fe270dcc738866a2efb48945f747a013e5f1b9bb
Ana Leite <negaanaleite@gmail.com>
2023-09-18 16:56:28 -0300

    Aula Estrururas de repetição

--- Controle-Fluxo/src/App.java
+++ Controle-Fluxo/src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        public static void main(String[] args) {
+            int numero = 1;
+
+        for (int x=1; x<2; x++){
+            numero = numero + x;
+        }
+        System.out.println("O valor de número é: " + numero);
     }
 }
+
